2c439f4d32329b8f6fb45d319063c5f0ba87818b
Trongnguyen29603 <115914589+Trongnguyen29603@users.noreply.github.com>
2022-11-18 08:33:33 +0700

    đổi trên dt

--- Models/pdo.php
+++ Models/pdo.php
@@ -3,7 +3,7 @@
  * Mở kết nối đến CSDL sử dụng PDO
  */
 function pdo_get_connection(){
-    $dburl = "mysql:host=localhost;dbname=duanmaunhom12;charset=utf8";
+    $dburl = "mysql:host=localhost;dbname=du-an1-nhom4;charset=utf8";
     $username = 'root';
     $password = '';
 
