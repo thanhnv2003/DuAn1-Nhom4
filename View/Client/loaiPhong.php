0f04d8916df3ad11224f4446bec40d7842988faa
thandinh <thandvph26474@fpt.edu.vn>
2022-11-25 12:46:41 +0700

    u

--- View/Client/loaiPhong.php
+++ View/Client/loaiPhong.php
@@ -34,66 +34,100 @@
                 <a href="index.php?url=dang-nhap"> <input type="button" value="Đăng nhập"></a>
             </div>
         </header>
-        <div class="">
+
+        <div class="banner">
             <div class="slideshow-container">
-                <div class="mySlides fade imgSlider">
+                <div class="mySlides fade">
                     <div class="numbertext">1 / 3</div>
                     <img src="View/src/image/chup-hinh-khach-san-nha-hang-mimosa-06-1 1.png" style="width:100%">
                     <div class="text">Caption Text</div>
                 </div>
 
-                <div class="mySlides fade imgSlider">
+                <div class="mySlides fade">
                     <div class="numbertext">2 / 3</div>
                     <img src="View/src/image/4 STUDIO 801_1 1920-min.jpg" style="width:100%">
                     <div class="text">Caption Two</div>
-            </div>    
+                </div>
+
+                <div class="mySlides fade">
+                    <div class="numbertext">3 / 3</div>
+                    <img src="View/src/image/5 DELUXE STUDIO_P704_1 1920-min.jpg" style="width:100%">
+                    <div class="text">Caption Three</div>
+                </div>
+
+                <a class="prev" onclick="plusSlides(-1)">❮</a>
+                <a class="next" onclick="plusSlides(1)">❯</a>
+
+            </div>
+            <br>
+
+            <div style="text-align:center">
+                <span class="dot" onclick="currentSlide(1)"></span>
+                <span class="dot" onclick="currentSlide(2)"></span>
+                <span class="dot" onclick="currentSlide(3)"></span>
+            </div>
+        </div>
+        <main>
             <div class="lp-product">
-              <?php
-               foreach ($list_roomss as $value) {
-                extract($value);
-                   echo '
+                <?php
+                foreach ($list_roomss as $value) {
+                    extract($value);
+                    echo '
                 <div class="lp-product1" >
                    <div class="lp-product-img" >
-                       <a href = "index.php?url=chi-tiet-phong&id='.$id_cate .'" ><img src = "./View/src/upload/'.$image.'" alt = "" class="img-room" ></a >
+                       <a href = "index.php?url=chi-tiet-phong&id=' . $id_cate . '" ><img src = "./View/src/upload/' . $image . '" alt = "" class="img-room" ></a >
                    </div >
                    <div class="lp-product-info" >
-                       <a href = "index.php?url=chi-tiet-phong&id='.$id_cate .'" >
-                           <h4>'.$name.'</h4 >
+                       <a href = "index.php?url=chi-tiet-phong&id=' . $id_cate . '" >
+                           <h4>' . $name . '</h4 >
                        </a >
-                       <span > Price :'.$price.'/ ngày </span >
+                       <span > Price :' . $price . '/ ngày </span >
                    </div >
                </div >
                 ';
                 }
-             ?>
-             </div>
-          </div>
-        </div>
-    </main>
-    <footer>
-        <div class="footer-box1">
-            <div class="footer-box">
-                <h2>Khách sạn CHAN MAY VN</h2>
-                <p><i class="fa-sharp fa-solid fa-location-dot"></i> Số 1 Đường Trịnh Văn Bô-Nam Từ Niêm -Hà Nội</p>
-                <p><i class="fa-sharp fa-solid fa-phone"></i> 0999999899</p>
-                <p><i class="fa-sharp fa-solid fa-envelope"></i> nhom4duan1@gmail.com</p>
+                ?>
             </div>
-            <div class="footer-box">
-                <h2>Về chúng tôi</h2>
-                <a href="index.php?url=lien-he">Liên hệ</a> <br>
-                <a href="#">Vị trí & chỉ đường</a> <br>
-                <a href="#">Tiện ích</a>
-            </div>
-            <div class="footer-box">
-                <h2>Những điều cần biết</h2> <br>
-                <a href="#">Hỏi đáp</a> <br>
-                <a href="#">Điều khoản & điều kiện</a>
-            </div>
-            <div class="footer-box2">
-                <a href=""> <img src="View/src/image/Xanh nước biển và Trắng Cổ điển Đường tròn Thời trang Biểu trưng Thời trang (95 × 98 px) 3.png" alt=""></a>
-                <script lang="javascript">var __vnp = { code: 4505, key: '', secret: '2d9af8430e9f03871b8868f5ddb7797c' }; (function () { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.defer = true; ga.src = '//core.vchat.vn/code/tracking.js'; var s = document.getElementsByTagName('script'); s[0].parentNode.insertBefore(ga, s[0]); })();
-                </script>
-            </div>
+
+        </main>
+        <footer>
+            <div class="footer-box1">
+                <div class="footer-box">
+                    <h2>Khách sạn CHAN MAY VN</h2>
+                    <p><i class="fa-sharp fa-solid fa-location-dot"></i> Số 1 Đường Trịnh Văn Bô-Nam Từ Niêm -Hà Nội</p>
+                    <p><i class="fa-sharp fa-solid fa-phone"></i> 0999999899</p>
+                    <p><i class="fa-sharp fa-solid fa-envelope"></i> nhom4duan1@gmail.com</p>
+                </div>
+                <div class="footer-box">
+                    <h2>Về chúng tôi</h2>
+                    <a href="index.php?url=lien-he">Liên hệ</a> <br>
+                    <a href="#">Vị trí & chỉ đường</a> <br>
+                    <a href="#">Tiện ích</a>
+                </div>
+                <div class="footer-box">
+                    <h2>Những điều cần biết</h2> <br>
+                    <a href="#">Hỏi đáp</a> <br>
+                    <a href="#">Điều khoản & điều kiện</a>
+                </div>
+                <div class="footer-box2">
+                    <a href=""> <img src="View/src/image/Xanh nước biển và Trắng Cổ điển Đường tròn Thời trang Biểu trưng Thời trang (95 × 98 px) 3.png" alt=""></a>
+                    <script lang="javascript">
+                        var __vnp = {
+                            code: 4505,
+                            key: '',
+                            secret: '2d9af8430e9f03871b8868f5ddb7797c'
+                        };
+                        (function() {
+                            var ga = document.createElement('script');
+                            ga.type = 'text/javascript';
+                            ga.async = true;
+                            ga.defer = true;
+                            ga.src = '//core.vchat.vn/code/tracking.js';
+                            var s = document.getElementsByTagName('script');
+                            s[0].parentNode.insertBefore(ga, s[0]);
+                        })();
+                    </script>
+                </div>
         </footer>
     </div>
     <script>
@@ -144,7 +178,7 @@
             }
             slides[slideIndex - 1].style.display = "block";
             dots[slideIndex - 1].className += " active";
-            setTimeout(showSlides, 2000); 
+            setTimeout(showSlides, 2000);
         }
     </script>
 </body>
