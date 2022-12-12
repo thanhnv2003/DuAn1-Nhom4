9eba82496c6196ad0c1bcbd9dcb6382c857cda19
Your Name <you@example.com>
2022-12-12 23:33:16 +0700

    liên hệ

--- View/Admin/lienhe/list_lienhe.php
+++ View/Admin/lienhe/list_lienhe.php
@@ -43,7 +43,7 @@
                             <th>Email</th>
                             <th>Số điện thoại</th>
                             <th>Nội dung</th>
-                            <th>trạng thái</th>
+                            <th>Trạng thái</th>
                             <th style="width: 22%;">Thao tác</th>
                         </tr>
                         </thead>
@@ -51,17 +51,29 @@
                         <?php
                         foreach ($list_contact as $value){
                             extract($value);
+                            if ($status == 0) {
+                                $trangthai = "Chưa trả lời";
+                            }elseif ($status == 1) {
+                                $trangthai = "Đã trả lời";                       
+                            }
+                            if ($sex == 1 ) {
+                               $gioitinh = "Nam";
+                            }elseif ($sex == 2) {
+                                $gioitinh = "Nữ";
+                            }else {
+                                $gioitinh = " Chưa chọn giới tính";
+                            }
                             echo '
                      
                         <tr>
                             <td><input class="form-check-input" type="checkbox" name="" id=""></td>
                             <td>' . $id_contact  . '</td>
-                            <td>' . $sex . '</td>
+                            <td>' . $gioitinh . '</td>
                             <td>'.$fullname.'</td>
                             <td>'.$email . '</td>
                             <td>' . $tel. '</td>
                             <td>' . $content . '</td>
-                            <td>'.$status.'</td>
+                            <td>'.$trangthai.' </td>                         
                             <td class="btn1">
                                 <a href="index.php?url=sua-lien-he&id='.$id_contact .'"><input class="btn btn-primary btn2" type="button" value="Sửa"></a>
                                 <a href="index.php?url=xoa-lien-he&id='.$id_contact .'" onclick="return confirm(`Bạn muốn xóa?`)" ; id="delete">
